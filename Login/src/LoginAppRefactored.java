fa7a4b54192f8eeb832200ec5892906d92003a60
Emmanuel P <deltron312@gmail.com>
2025-03-28 12:54:00 +0100

    eliminacion de codigo

--- Login/src/LoginAppRefactored.java
+++ Login/src/LoginAppRefactored.java
@@ -15,18 +15,3 @@ public class LoginAppRefactored {
         });
     }
 }
-// public class LoginAppRefactored {
-//     public static void main(String[] args) {
-//         // Configuración de credenciales
-//         AuthenticationService authService = new AuthenticationService(
-//             "Emmanuel Pallares",
-//             "220610"
-//         );
-        
-//         // Creación de la interfaz gráfica
-//         SwingUtilities.invokeLater(() -> {
-//             LoginUI loginUI = new LoginUI(authService);
-//             loginUI.display();
-//         });
-//     }
-// }
